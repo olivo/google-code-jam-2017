76f07298ff7702fee1791180fa6f30298a2ecc0f
Oswaldo Olivo <osolivo@microsoft.com>
2018-03-10 11:29:00 -0800

    Implemented O(K*log(N)) solution to Bathroom Stalls.

--- bathroom-stalls.py
+++ bathroom-stalls.py
@@ -1,4 +1,6 @@
-file = open("C-small-practice-2.in", "r")
+import heapq
+
+file = open("C-large-practice.in", "r")
 
 num_tests = int(file.readline().strip())
 
@@ -7,60 +9,35 @@ for test in range(num_tests):
     N = int(N)
     K = int(K)
 
-    stalls = [(0, 0, 0) for x in range(N + 2)]
+    q = []
 
-    stalls[0] = (1, 0, 0)
-    stalls[N + 1] = (1, 0, 0)
 
-    for i in range(1, N + 1):
-        stalls[i] = (0, i - 1, N - i)
+    heapq.heappush(q, ((N, 1), (1, N)))
 
-    #if test == 0:
-    #    print("\n\nTest: " + str(test))
     for i in range(K):
-        #if test == 0:
-        #    print(stalls)
-        best_val = -1
-        best_index = -1
+        #print("Intervals: " + str(q))
 
-        for j in range(1, N + 1):
-            if stalls[j][0] == 0 and best_val < min(stalls[j][1], stalls[j][2]):
-                best_val = min(stalls[j][1], stalls[j][2])
-                best_index = j
+        q_element = heapq.heappop(q)[1]
 
-        candidate_slots = []
+        interval_len = q_element[1] - q_element[0] + 1
+        current_left_ls = 0
+        current_right_ls = 0
 
-        for j in range(1, N + 1):
-            if min(stalls[j][1], stalls[j][2]) == best_val:
-                candidate_slots.append((j, stalls[j]))
+        #print("Interval: " + str(q_element))
+        #print("Interval length: " + str(interval_len))
 
-        best_val = -1
-        best_index = -1
+        if interval_len > 1:
+            midpoint = (q_element[0] + q_element[1]) // 2
+            #print("Start: " + str(q_element[0]))
+            #print("End: " + str(q_element[1]))
+            #print("Midpoint: " + str(midpoint))
 
-        for index, stall in candidate_slots:
-            if max(stall[1], stall[2]) > best_val and stall[0] == 0:
-                best_val = max(stall[1], stall[2])
-                best_index = index
+            if q_element[0] < midpoint:
+                heapq.heappush(q, ((-(midpoint - q_element[0]), q_element[0]),(q_element[0], midpoint - 1)))
+                current_left_ls = midpoint - q_element[0]
+            if q_element[1] > midpoint:
+                heapq.heappush(q, ((-(q_element[1] - midpoint), midpoint + 1),(midpoint + 1, q_element[1])))
+                current_right_ls = q_element[1] - midpoint
 
-        stalls[best_index] = (1, stalls[best_index][1], stalls[best_index][2])
-
-        if i == K - 1:
-            max_val = max(stalls[best_index][1], stalls[best_index][2])
-            min_val = min(stalls[best_index][1], stalls[best_index][2])
-            break
-
-        for left_index in range(best_index - 1, -1, -1):
-            stalls[left_index] = (stalls[left_index][0],
-                                  stalls[left_index][1],
-                                  min(best_index - left_index - 1, stalls[left_index][2]))
-            if stalls[left_index][0] == 1:
-                break
-
-        for right_index in range(best_index + 1, N + 1):
-            stalls[right_index] = (stalls[right_index][0],
-                                   min(right_index - best_index - 1, stalls[right_index][1]),
-                                   stalls[right_index][2])
-            if stalls[right_index][0] == 1:
-                break
-
-    print("Case #" + str(test + 1) + ": " + str(max_val) + " " + str(min_val))
+    print("Case #" + str(test + 1) + ": " + str(max(current_left_ls, current_right_ls)) \
+                                          + " " + str(min(current_left_ls, current_right_ls)))
